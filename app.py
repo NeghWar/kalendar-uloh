c26bdf617045380ff1624f93d571e203c43e63a8
NeghWar <p.praznovsky@gmail.com>
2026-09-15 21:36:53 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -337,7 +337,6 @@ with tab_zoznam:
                             new_rev = st.date_input("Posledná Revízia:", curr_rev)
                             new_rev_sk = st.date_input("Posledná Revízna skúška:", curr_rev_sk)
                             
-                            # Opravené zoznamy periód
                             list_rev = [2, 3]
                             p_rev_index = list_rev.index(stroj['perioda_reviznej_skusky']) if stroj['perioda_reviznej_skusky'] in list_rev else 0
                             new_p_rev = st.selectbox("Perióda Revíznej skúšky (roky):", list_rev, index=p_rev_index)
@@ -380,7 +379,7 @@ with tab_zoznam:
                             "nasledujuca_podrobna_prehliadka_ok": n_pod_ok.isoformat() if n_pod_ok else None,
                             "posledna_uradna_skuska": new_urad.isoformat() if new_urad else None,
                             "perioda_uradnej_skusky": new_p_urad,
-                            "nasledujuca_uradna_skuska": n_urad.isoformat() if n_urad =="" else None,
+                            "nasledujuca_uradna_skuska": n_urad.isoformat() if new_urad else None,
                             "posledna_odborna_prehliadka": new_odb_pr.isoformat() if new_odb_pr else None,
                             "nasledujuca_odborna_prehliadka": n_odb_pr.isoformat() if n_odb_pr else None,
                             "posledna_odborna_skuska": new_odb_sk.isoformat() if new_odb_sk else None,
@@ -393,3 +392,4 @@ with tab_zoznam:
                         try:
                             supabase.table("stroje").update(upravene_data).eq("id", stroj["id"]).execute()
                             st.success("Zmeny boli úspešne uložené do cloudu!")
+                            st.session_state[f"editovanie_{stroj['id']}"] = False
